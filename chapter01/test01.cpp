ff5d2f0a4e2b473224e9c638521be0d9ebf6de99
kuan <kitschxcy@outlook.com>
2024-07-15 12:02:53 +0800

    Initial commit

--- chapter01/test01.cpp
+++ chapter01/test01.cpp
@@ -10,7 +10,7 @@ int main() {
     array2 arr1 = {0};
     array2 arr2 = {0};
 
-    int x = MAX_NUM * (MAX_NUM + 1) / 2;
+    const int x = MAX_NUM * (MAX_NUM + 1) / 2;
     int arr3[x];
     int arr4[x];
 
